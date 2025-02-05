505ec726a9786806f1598c812219f6841ab5e030
gyamfiphilemon84 <gyamfiphilemon84@gmail.com>
2025-02-05 05:49:30 +0100

    Add files via upload
    Adding index.html, style.css, & script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  document.getElementById("recommend_btn").onclick = addRecommendation;
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "Thanks for leaving a recommendation!") {
+    console.log("New recommendation added");
+    //Call showPopup here
+    showPopup("Thanks for leaving a recommendation!");
+    // Create a new 'recommendation' element and set it's value to the user's message
+    var element = document.createElement("div");
+    element.setAttribute("class","recommendation");
+    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    // Add this element to the end of the list of recommendations
+    document.getElementById("all_recommendations").appendChild(element); 
+    
+    // Reset the value of the textarea
+    recommendation.value = "";
+  }
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
